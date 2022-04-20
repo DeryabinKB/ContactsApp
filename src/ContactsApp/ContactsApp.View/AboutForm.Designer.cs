77a3fcd5789a16e19c545d0898243579593f7cd4
Никита Дерябин <deryabin.2001@inbox.ru>
2022-04-20 16:52:41 +0700

    fixed (ui): Переименованы недостающие лэйблы

--- src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
+++ src/ContactsApp/ContactsApp.View/AboutForm.Designer.cs
@@ -29,99 +29,99 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.AppName = new System.Windows.Forms.Label();
-            this.Version = new System.Windows.Forms.Label();
-            this.Author = new System.Windows.Forms.Label();
-            this.FeedbackMail = new System.Windows.Forms.Label();
-            this.Github = new System.Windows.Forms.Label();
-            this.MailLink = new System.Windows.Forms.LinkLabel();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.AppNameLable = new System.Windows.Forms.Label();
+            this.VersionLable = new System.Windows.Forms.Label();
+            this.AuthorLable = new System.Windows.Forms.Label();
+            this.FeedbackMailLable = new System.Windows.Forms.Label();
+            this.GithubLable = new System.Windows.Forms.Label();
+            this.MailLinkLable = new System.Windows.Forms.LinkLabel();
+            this.GitLinkLable = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
-            // AppName
+            // AppNameLable
             // 
-            this.AppName.AutoSize = true;
-            this.AppName.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.AppName.Location = new System.Drawing.Point(12, 9);
-            this.AppName.Name = "AppName";
-            this.AppName.Size = new System.Drawing.Size(194, 41);
-            this.AppName.TabIndex = 0;
-            this.AppName.Text = "ContactsApp";
+            this.AppNameLable.AutoSize = true;
+            this.AppNameLable.Font = new System.Drawing.Font("Segoe UI Semibold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AppNameLable.Location = new System.Drawing.Point(12, 9);
+            this.AppNameLable.Name = "AppNameLable";
+            this.AppNameLable.Size = new System.Drawing.Size(194, 41);
+            this.AppNameLable.TabIndex = 0;
+            this.AppNameLable.Text = "ContactsApp";
             // 
-            // Version
+            // VersionLable
             // 
-            this.Version.AutoSize = true;
-            this.Version.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Version.Location = new System.Drawing.Point(14, 50);
-            this.Version.Name = "Version";
-            this.Version.Size = new System.Drawing.Size(77, 30);
-            this.Version.TabIndex = 1;
-            this.Version.Text = "v. 1.0.0";
+            this.VersionLable.AutoSize = true;
+            this.VersionLable.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.VersionLable.Location = new System.Drawing.Point(14, 50);
+            this.VersionLable.Name = "VersionLable";
+            this.VersionLable.Size = new System.Drawing.Size(77, 30);
+            this.VersionLable.TabIndex = 1;
+            this.VersionLable.Text = "v. 1.0.0";
             // 
-            // Author
+            // AuthorLable
             // 
-            this.Author.AutoSize = true;
-            this.Author.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Author.Location = new System.Drawing.Point(15, 120);
-            this.Author.Name = "Author";
-            this.Author.Size = new System.Drawing.Size(182, 21);
-            this.Author.TabIndex = 2;
-            this.Author.Text = "Author: Deryabin Nikita";
+            this.AuthorLable.AutoSize = true;
+            this.AuthorLable.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AuthorLable.Location = new System.Drawing.Point(15, 120);
+            this.AuthorLable.Name = "AuthorLable";
+            this.AuthorLable.Size = new System.Drawing.Size(182, 21);
+            this.AuthorLable.TabIndex = 2;
+            this.AuthorLable.Text = "Author: Deryabin Nikita";
             // 
-            // FeedbackMail
+            // FeedbackMailLable
             // 
-            this.FeedbackMail.AutoSize = true;
-            this.FeedbackMail.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.FeedbackMail.Location = new System.Drawing.Point(15, 188);
-            this.FeedbackMail.Name = "FeedbackMail";
-            this.FeedbackMail.Size = new System.Drawing.Size(158, 21);
-            this.FeedbackMail.TabIndex = 3;
-            this.FeedbackMail.Text = "e-mail for feedback:";
+            this.FeedbackMailLable.AutoSize = true;
+            this.FeedbackMailLable.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FeedbackMailLable.Location = new System.Drawing.Point(15, 188);
+            this.FeedbackMailLable.Name = "FeedbackMailLable";
+            this.FeedbackMailLable.Size = new System.Drawing.Size(158, 21);
+            this.FeedbackMailLable.TabIndex = 3;
+            this.FeedbackMailLable.Text = "e-mail for feedback:";
             // 
-            // Github
+            // GithubLable
             // 
-            this.Github.AutoSize = true;
-            this.Github.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Github.Location = new System.Drawing.Point(15, 222);
-            this.Github.Name = "Github";
-            this.Github.Size = new System.Drawing.Size(66, 21);
-            this.Github.TabIndex = 4;
-            this.Github.Text = "GitHub:";
+            this.GithubLable.AutoSize = true;
+            this.GithubLable.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GithubLable.Location = new System.Drawing.Point(15, 222);
+            this.GithubLable.Name = "GithubLable";
+            this.GithubLable.Size = new System.Drawing.Size(66, 21);
+            this.GithubLable.TabIndex = 4;
+            this.GithubLable.Text = "GitHub:";
             // 
-            // MailLink
+            // MailLinkLable
             // 
-            this.MailLink.AutoSize = true;
-            this.MailLink.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MailLink.Location = new System.Drawing.Point(179, 188);
-            this.MailLink.Name = "MailLink";
-            this.MailLink.Size = new System.Drawing.Size(187, 21);
-            this.MailLink.TabIndex = 5;
-            this.MailLink.TabStop = true;
-            this.MailLink.Text = "deryabin.2001@inbox.ru";
+            this.MailLinkLable.AutoSize = true;
+            this.MailLinkLable.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MailLinkLable.Location = new System.Drawing.Point(179, 188);
+            this.MailLinkLable.Name = "MailLinkLable";
+            this.MailLinkLable.Size = new System.Drawing.Size(187, 21);
+            this.MailLinkLable.TabIndex = 5;
+            this.MailLinkLable.TabStop = true;
+            this.MailLinkLable.Text = "deryabin.2001@inbox.ru";
             // 
-            // linkLabel1
+            // GitLinkLable
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.linkLabel1.Location = new System.Drawing.Point(87, 222);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(96, 21);
-            this.linkLabel1.TabIndex = 6;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "DeryabinKB";
+            this.GitLinkLable.AutoSize = true;
+            this.GitLinkLable.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.GitLinkLable.Location = new System.Drawing.Point(87, 222);
+            this.GitLinkLable.Name = "GitLinkLable";
+            this.GitLinkLable.Size = new System.Drawing.Size(96, 21);
+            this.GitLinkLable.TabIndex = 6;
+            this.GitLinkLable.TabStop = true;
+            this.GitLinkLable.Text = "DeryabinKB";
             // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 307);
-            this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.MailLink);
-            this.Controls.Add(this.Github);
-            this.Controls.Add(this.FeedbackMail);
-            this.Controls.Add(this.Author);
-            this.Controls.Add(this.Version);
-            this.Controls.Add(this.AppName);
+            this.Controls.Add(this.GitLinkLable);
+            this.Controls.Add(this.MailLinkLable);
+            this.Controls.Add(this.GithubLable);
+            this.Controls.Add(this.FeedbackMailLable);
+            this.Controls.Add(this.AuthorLable);
+            this.Controls.Add(this.VersionLable);
+            this.Controls.Add(this.AppNameLable);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(452, 346);
             this.MinimumSize = new System.Drawing.Size(452, 346);
@@ -134,12 +134,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Label AppName;
-        private System.Windows.Forms.Label Version;
-        private System.Windows.Forms.Label Author;
-        private System.Windows.Forms.Label FeedbackMail;
-        private System.Windows.Forms.Label Github;
-        private System.Windows.Forms.LinkLabel MailLink;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label AppNameLable;
+        private System.Windows.Forms.Label VersionLable;
+        private System.Windows.Forms.Label AuthorLable;
+        private System.Windows.Forms.Label FeedbackMailLable;
+        private System.Windows.Forms.Label GithubLable;
+        private System.Windows.Forms.LinkLabel MailLinkLable;
+        private System.Windows.Forms.LinkLabel GitLinkLable;
     }
 }
