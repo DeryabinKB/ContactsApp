515af08d8f0ec9891d1cf8bbfa9c1311cdae86a3
Никита Дерябин <deryabin.2001@inbox.ru>
2022-06-23 19:07:19 +0700

    Исправления багов

--- src/ContactsApp/ContactsApp.Model/ProjectManager.cs
+++ src/ContactsApp/ContactsApp.Model/ProjectManager.cs
@@ -18,8 +18,7 @@ namespace ContactsApp.Model
         /// <summary>
         /// Папка.
         /// </summary>
-        private static readonly string _folder = $"{Environment.
-            GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\DNA\\ContactsApp\\";
+        private static readonly string _folder = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}\\DNA\\ContactsApp\\";
 
         /// <summary>
         /// Путь сохранения файла.
